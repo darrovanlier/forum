d7480376f907549f2b4d0122c795c82d4c24dc77
Darro <darrovanlier@gmail.com>
2017-06-02 11:02:39 +0200

    update
    ofc

--- src/app/indexhandler.php
+++ src/app/indexhandler.php
@@ -1,1 +1,3 @@
 <?php
+$fetch_themes = $dbh->prepare('select * from themes order by id asc');
+$fetch_themes->execute();
--- src/theme.php
+++ src/theme.php
@@ -68,30 +68,30 @@ include('app/indexhandler.php');
                 <h2 class="text-center">Themes</h2>
                 <table class="table table-striped table-bordered table-hover table-responsive">
                     <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Last Post</th>
-                            <th>Created by</th>
-                            <th>Topic Count</th>
-                        </tr>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Last Post</th>
+                        <th>Created by</th>
+                        <th>Topic Count</th>
+                    </tr>
                     </thead>
-                 <tbody>
-                  <tr>
-                    <?php
-                    if ($fetch_themes->rowCount() > 0) {
-                        $rows = $fetch_themes->fetchAll();
-                        foreach ($rows as $row) {
-                            $id = $row['id'];
-                            echo '<td><a href="theme.php?id='.$id.'">'.$row['title'].'</td>';
-                            echo '<td>'.$row['description'].'</td>';
-                            echo '<td>Laatste post</td>';
-                            echo '<td>'.$row['user_id'].' '.$row['created_at'].' </td>';
-                            echo '<td>topic count</td>';
-                            echo '</tr>';
+                    <tbody>
+                    <tr>
+                        <?php
+                        if ($fetch_themes->rowCount() > 0) {
+                            $rows = $fetch_themes->fetchAll();
+                            foreach ($rows as $row) {
+                                $id = $row['id'];
+                                echo '<td><a href="theme.php?id='.$id.'">'.$row['title'].'</td>';
+                                echo '<td>'.$row['description'].'</td>';
+                                echo '<td>Laatste post</td>';
+                                echo '<td>'.$row['user_id'].' '.$row['created_at'].' </td>';
+                                echo '<td>topic count</td>';
+                                echo '</tr>';
+                            }
                         }
-                    }
-                    ?>
+                        ?>
 
                     </tbody>
                 </table>
